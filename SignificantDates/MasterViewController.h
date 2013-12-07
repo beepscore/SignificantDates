3fdb5eb3e7d860e6af90b4dc56edd800fe3df31e
beepscore <support@beepscore.com>
2013-12-07 15:16:53 -0800

    Initial Commit

---
+++ SignificantDates/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/7/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ SignificantDates/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/7/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
