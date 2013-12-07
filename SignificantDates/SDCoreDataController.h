ff30090d1f0f313e41d50303f659192cf0da3d91
beepscore <support@beepscore.com>
2013-12-07 15:46:33 -0800

    In Xcode drag README.md from Finder into project navigator. Add class SDCoreDataController.

---
+++ SignificantDates/SDCoreDataController.h
@@ -0,0 +1,13 @@
+//
+//  SDCoreDataController.h
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/7/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDCoreDataController : NSObject
+
+@end
---
+++ SignificantDates/SDCoreDataController.m
@@ -0,0 +1,13 @@
+//
+//  SDCoreDataController.m
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/7/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "SDCoreDataController.h"
+
+@implementation SDCoreDataController
+
+@end
