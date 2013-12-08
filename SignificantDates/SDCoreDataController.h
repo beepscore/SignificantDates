1106c89eb4c55e11db4174ee1643d4d110b0a6c8
beepscore <support@beepscore.com>
2013-12-07 16:42:59 -0800

    In AppDelegate delete CoreData code. In SDCoreDataController add CoreData code as in reference tutorial project. Delete template view controllers and storyboards. In project settings don’t reference deleted storyboards.

--- SignificantDates/SDCoreDataController.h
+++ SignificantDates/SDCoreDataController.h
@@ -5,9 +5,23 @@
 //  Created by Steve Baker on 12/7/13.
 //  Copyright (c) 2013 Beepscore LLC. All rights reserved.
 //
+//  Reference SDCoreDataController.h created by Chris Wagner on 5/14/12.
+//  http://www.raywenderlich.com/15916/how-to-synchronize-core-data-with-a-web-service-part-1
 
 #import <Foundation/Foundation.h>
 
 @interface SDCoreDataController : NSObject
+
++ (id)sharedInstance;
+
+- (NSURL *)applicationDocumentsDirectory;
+
+- (NSManagedObjectContext *)masterManagedObjectContext;
+- (NSManagedObjectContext *)backgroundManagedObjectContext;
+- (NSManagedObjectContext *)newManagedObjectContext;
+- (void)saveMasterContext;
+- (void)saveBackgroundContext;
+- (NSManagedObjectModel *)managedObjectModel;
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator;
 
 @end
