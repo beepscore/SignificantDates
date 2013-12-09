00d27fb75b9c4649fd5727b09e36f4b288a86a23
beepscore <support@beepscore.com>
2013-12-08 19:46:28 -0800

    Add BSParseDotComRequestSerializer

---
+++ SignificantDates/Networking/BSParseDotComRequestSerializer.h
@@ -0,0 +1,13 @@
+//
+//  BSParseDotComRequestSerializer.h
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/8/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "AFURLRequestSerialization.h"
+
+@interface BSParseDotComRequestSerializer : AFJSONRequestSerializer
+
+@end
---
+++ SignificantDates/Networking/BSParseDotComRequestSerializer.m
@@ -0,0 +1,13 @@
+//
+//  BSParseDotComRequestSerializer.m
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/8/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "BSParseDotComRequestSerializer.h"
+
+@implementation BSParseDotComRequestSerializer
+
+@end
