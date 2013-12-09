539537b1fac179f5059eec384346cf2bf5004688
beepscore <support@beepscore.com>
2013-12-08 16:37:24 -0800

    Add BSHTTPSessionManager as subclass of AFHTTPSessionManager

---
+++ SignificantDates/BSHTTPSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  BSHTTPSessionManager.h
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/8/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface BSHTTPSessionManager : AFHTTPSessionManager
+
+@end
---
+++ SignificantDates/BSHTTPSessionManager.m
@@ -0,0 +1,13 @@
+//
+//  BSHTTPSessionManager.m
+//  SignificantDates
+//
+//  Created by Steve Baker on 12/8/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "BSHTTPSessionManager.h"
+
+@implementation BSHTTPSessionManager
+
+@end
